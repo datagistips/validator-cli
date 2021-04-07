ec1a012762932355e6fbee519f4f4a90d37fc932
datagistips <mathieu.rajerison@gmail.com>
2021-04-07 18:47:33 +0200

    mode control

--- validator-cli.py
+++ validator-cli.py
@@ -13,10 +13,7 @@ import csv
 global mapping
 
 
-def transform(input_data, input_mapping, output_data = None):
-	
-	
-	# READ DATA ###############################################
+def read_data(input_data):
 	
 	input_name = re.search('(.*)\\.(.*)', input_data).group(1)
 	input_extension = re.search('(.*)\\.(.*)', input_data).group(2)
@@ -27,7 +24,71 @@ def transform(input_data, input_mapping, output_data = None):
 	else:
 		file_class = "geo"
 		data = gpd.read_file(input_data, encoding = 'utf-8')
+			
+	return(data)
+
+
+def control(input_data, input_mapping):
+	
+	data = read_data(input_data)
+	mapping  = pd.read_csv(input_mapping)
+	
+	# Columns
+	data_columns = list(data.columns)
+	schema_columns = list(mapping.iloc[:,0])
+	
+	# Schema columns absent from data
+	strange_cols = list()
+	matching_cols = list()
+	for elt in schema_columns:
+		if elt not in data_columns:
+			strange_cols.append(elt)
+		else:
+			matching_cols.append(elt)
 		
+	# Data columns absent from schema
+	strange_cols = list()
+	matching_cols = list()
+	for elt in data_columns:
+		if elt not in schema_columns:
+			strange_cols.append(elt)
+		else:
+			matching_cols.append(elt)
+	
+	# Conform or not ?
+	if len(matching_cols) > 0:
+		conform = False
+		pc = len(matching_cols) / data.shape[0]
+	else:
+		conform = True
+	
+	# Messages
+	print('[KO] Data non valid' if conform is False else '[OK] Data valid')
+	print('---')
+	print('Input data : %s'%input_data)
+	print('Data schema : %s'%input_mapping)
+	print('---')
+	print(('Schema columns : %s')%(', '.join(schema_columns)))
+	print(('Data columns : %s')%(', '.join(data_columns)))
+	print('---')
+	print(('Schema columns present in data : %s')%(', '.join(matching_cols)))
+	print(('Schema columns absent from data : %s')%(', '.join(strange_cols)))
+	print('---')
+	print(('Data columns present in schema : %s')%(', '.join(matching_cols)))
+	print(('Data columns absent from schema : %s')%(', '.join(strange_cols)))
+
+
+def transform(input_data, input_mapping, output_data = None):
+	
+	
+	# READ DATA ###############################################
+	
+	data = read_data(input_data)
+	mapping  = pd.read_csv(input_mapping)
+	
+	input_name = re.search('(.*)\\.(.*)', input_data).group(1)
+	input_extension = re.search('(.*)\\.(.*)', input_data).group(2)
+	
 		
 	# CONTROL ###################################################
 	
@@ -86,11 +147,11 @@ if __name__ == "__main__":
 	
 	parser = argparse.ArgumentParser()
 	
-	parser.add_argument("mode", choices=['control', 'transform'], help='Control data against a data schema or transform data thanks to a mapping file')
+	parser.add_argument("mode", choices=['control', 'transform'], help="'control' data against a data schema or 'transform' data thanks to a mapping file")
 	parser.add_argument("-d", "--directory", help="process entire directory", action="store_true")
 	parser.add_argument("input", help="input file or directory (depends if you specified -d or not)")
-	parser.add_argument("schema", help="input mapping file or data schema file path")
-	parser.add_argument("-o", "--output", help="Output file (Optional and active if in single file mode, not directory mode)")
+	parser.add_argument("schema", help="data schema file path (if 'control' mode) or mapping file (if 'transform' mode)")
+	# ~ parser.add_argument("-o", "--output", help="Output file (Optional and active if in single file mode, not directory mode)")
 
 
 	args = parser.parse_args()
@@ -99,11 +160,8 @@ if __name__ == "__main__":
 	directory = args.directory
 	input_data = args.input
 	input_mapping = args.schema
-	output_data = args.output
-	
-	print(mode)
-	print(args)
-	
+	# ~ output_data = args.output
+	output_data = None
 	
 	# READ ####################################################
 	
@@ -111,33 +169,44 @@ if __name__ == "__main__":
 	if not os.path.exists(input_mapping):
 		print(("ERROR : mapping file '%s' doesn't exist")%input_mapping)
 		quit()
-	else:
-		mapping  = pd.read_csv(input_mapping)
+		
+	# CONTROL ########################################################"
+	if not os.path.exists(input_data):
+		print(("ERROR : file '%s' doesn't exist")%input_data)
+		quit()
 
 	
 	# PROCESS ####################################################
 	
 	# Transform
 	
-	if mode == "transform":
+	if mode == "control":
+		control(input_data, input_mapping)
+		
+		# Single file treatment -----
 		if directory is False :
-			
-			# READ FILE ----
-			
-			if not os.path.exists(input_data):
-				print(("ERROR : file '%s' doesn't exist")%input_data)
-				quit()
+			control(input_data, input_mapping) # we'll create and output data name based on input data file name
+		
+		# Directory treatment -----
+		elif directory is True:			
+				
+			l = os.listdir(input_data)
+			for elt in l:
+				control(os.path.join(input_data, elt), input_mapping)
+				print('-----------------------------------------------------------------------------------')
+				
+	elif mode == "transform":
+		
+		# Single file treatment -----
+		if directory is False :
 			
 			if output_data is not None:
 				transform(input_data, input_mapping, output_data) # we use the output data file name
 			else:
 				transform(input_data, input_mapping) # we'll create and output data name based on input data file name
-			
+		
+		# Directory treatment -----
 		elif directory is True:
-			
-			if not os.path.exists(input_data):
-				print(("ERROR : directory '%s' doesn't exist")%input_dir)
-				quit()
 			
 			if output_data is not None:
 				print("Output file will not be taken into account when renaming batches of data")
