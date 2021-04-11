c0eb5e2fe1dae5014e1571267420ddf0f808500f
datagistips <mathieu.rajerison@gmail.com>
2021-04-11 19:37:16 +0200

    utf-8

--- tests-py/inspect-type.py
+++ tests-py/inspect-type.py
@@ -1,4 +1,4 @@
-# -*- coding: iso-8859-1 -*-
+# -*- coding: utf-8 -*-
 
 # - cr�er fonction consid�rant le mapping control_types_mapping(data, mapping, standard)
 # - am�liorer la d�tection des dates
