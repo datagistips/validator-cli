9126cfea64e092126641a1022887546ebee8ff95
datagistips <mathieu.rajerison@gmail.com>
2021-04-13 11:59:22 +0200

    update readme.md

--- examples/control.bat
+++ examples/control.bat
@@ -1,6 +1,6 @@
 @echo off
 
 :: example with errors, patterns and enum lists of values
-C:\Python39\python -i validator-cli.py control examples\datasets\data3.csv examples\datasets\schema3.csv
+C:\Python39\python validator-cli.py control examples\datasets\data3.csv examples\datasets\schema3.csv
 
 pause
